103fa460c6dcbf18ef76cdf6b18b41da25104b14
LiamTeixido <liamteixido@gmail.com>
2023-05-22 22:08:39 -0500

    Correción pequeña - Liam Teixido

--- Assets/Scripts/IObserver.cs
+++ Assets/Scripts/IObserver.cs
@@ -1,20 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
-public class IObserver : MonoBehaviour
+public interface IObserver
 {
-    private TextMeshProUGUI scoreText;
-
-    private void Start()
-    {
-        scoreText = GetComponent<TextMeshProUGUI>();
-    }
-
-    private void Update()
-    {
-        scoreText.text = "Score: " + ScoreManager.Instance.Score.ToString();
-    }
+    public void Execute(ISubject
+    subject);
 }
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Observer : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + ScoreManager.Instance.Score.ToString();
+    }
+}
